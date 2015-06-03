650df920beaf026397e671a98cf21fad5c37797b
Brentley Jones <github@brentleyjones.com>
2015-06-02 23:47:15 -0500

    Add visible anchors to headers

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+/* Anchors */
+
+anchors.options.placement = 'left';
+anchors.add('.post h2');
+anchors.add('.post h3');
+anchors.add('.post h4');
+anchors.add('.post h5');
+anchors.add('.post h6');
